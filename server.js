2eaea453f2ca9c56e518311e65904ed6941007d9
Luis Felipe <1luisfelipe2@gmail.com>
2022-02-06 22:24:14 -0400

    Heroku fix3

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express=require('express');
-const path=require('path');
+// importar express
+const express = require('express');
+// iniciar express
 const app = express();
+// nome da pasta no dist que sera feito o build
+const appName = 'angular-material-schematics';
+// local onde build ira gerar os arquivos
+const outputPath = `${__dirname}/dist/${appName}`;
 
-const PORT = process.env.PORT || 8080;
-
-app.use(express.static(__dirname + '/dist/benedictas-header-com-sidenav'));
-
-app.get('/*',(req, res) =>{
-        res.sendFile(__dirname + '/dist/benedictas-header-com-sidenav/index.html');
+// seta o diretorio de build para servir o conteudo Angular
+app.use(express.static(outputPath));
+// redirecionar qualquer requisicao para o index.html
+app.get('/*', (req, res) => {
+  res.sendFile(`${outputPath}/index.html`);
 });
-
-app.listen(PORT,()=>{
-    console.log('Servidor rodando na porta' + PORT);
-})
+// ouvir a porta que o Heroku disponibilizar
+app.listen(process.env.PORT);
